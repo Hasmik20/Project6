b41bfee1e9df7735bcede2cf16005ff0233cc2aa
Hasmik20 <h_aghayan@yahoo.com>
2020-05-21 15:14:33 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,100 @@
+const qwerty = document.querySelector('#qwerty')
+const phrase = document.querySelector('#phrase')
+const startGame = document.querySelector('.btn__reset')
+const overlay = document.querySelector('#overlay')
+let missed = 0;
+let heart = document.querySelectorAll('img')
+
+
+const phrases =[
+    'burst your bubble',
+    'cup of joe',
+    'a dime a dozen',
+    'a piece of cake',
+    'cut to the chase'
+]
+
+//Create a getRandomPhraseAsArray function.
+const getRandomPhraseAsArray = arr =>{
+    let i = Math.floor(Math.random() * arr.length);
+    return arr[i]
+}
+
+getRandomPhraseAsArray(phrases);
+
+
+
+//Set the game display.
+const addPhraseToDisplay = arr =>{
+        for(let i = 0; i < arr.length; i++){
+            const ul = document.querySelector('#phrase ul')
+            const li = document.createElement('li')
+            ul.appendChild(li)
+            li.textContent += arr[i]
+        if(li.textContent !== " "){
+            li.className = "letter"
+        }else{
+            li.className = "space"
+        }  
+    }
+}
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray); 
+
+
+//Attach a event listener to the “Start Game” button to hide the start screen overlay.
+startGame.addEventListener('click', () => {
+    overlay.style.display = "none"
+})
+
+
+//Create a checkLetter function
+
+const checkLetter = button =>{
+    let match = null;
+    const letter = document.querySelectorAll('.letter')
+         for(let i = 0; i < letter.length; i++){
+          if(button === letter[i].textContent.toLowerCase()){
+            letter[i].className = "show"
+            match = true
+        }
+    }
+    return match;
+}
+
+ //addEventListener to the keyboard
+
+qwerty.addEventListener('click',(e) => {
+         if(e.target.tagName === 'BUTTON'){
+             e.target.className = 'chosen'
+    } 
+    const letterFound = checkLetter(e.target.textContent)
+    if(letterFound === null && e.target.tagName === 'BUTTON' ){
+        heart = 5;
+        heart--;
+        missed++; 
+     }
+ checkWin(checkLetter)
+})
+
+ //Create a checkWin function
+    const checkWin = () =>{
+        const letter = document.querySelectorAll('.letter')
+        const show = document.querySelectorAll('.show')
+        const title = document.querySelector('.title')
+        if(letter.length === show.length){
+            overlay.className ='win';
+            title.textContent = "You win"
+            overlay.style.display = "flex"
+        }else if(missed >= 4){
+            overlay.className ='lose';
+            title.textContent = "You lost"
+            overlay.style.display = "flex"
+            overlay.textContent = "Try again"
+        }
+        
+   }
+
+
+
+
